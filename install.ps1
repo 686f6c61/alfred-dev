1cd33da53dd287d63eb7ef47bf4e544754538500
686f6c61 <github@00b.tech>
2026-03-14 14:17:47 +0100

    chore: bump version a 0.4.2

--- install.ps1
+++ install.ps1
@@ -19,7 +19,7 @@ $ErrorActionPreference = 'Stop'
 
 $Repo = "686f6c61/alfred-dev"
 $PluginName = "alfred-dev"
-$Version = "0.4.1"
+$Version = "0.4.2"
 
 # -- Funciones auxiliares ---------------------------------------------------
 
